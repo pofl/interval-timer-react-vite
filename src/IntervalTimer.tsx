b727c0e6db174747b56247d07efb76f9ca81318d
Florian Polster <florian.polster@staffbase.com>
2025-03-16 12:57:15 +0100

    fixup Port JS

--- src/IntervalTimer.tsx
+++ src/IntervalTimer.tsx
@@ -14,17 +14,17 @@ const SettingControl: React.FC<SettingControlProps> = ({ value, onChange, label 
     <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', gap: '5px' }} className='marg'>
       <button className='pad-s fsize-m' onClick={() => onChange(value - 5)}>-5</button>
       <button className='pad-s fsize-m' onClick={() => onChange(value - 1)}>-1</button>
-        <input
-          style={{
-            flexGrow: 1,
-            flexBasis: 0,
-            minWidth: '2em',
-          }}
-          className='pad-s fsize-m'
-          type="number"
-          value={value}
-          onChange={(e) => onChange(parseInt(e.target.value))}
-        />
+      <input
+        style={{
+          flexGrow: 1,
+          flexBasis: 0,
+          minWidth: '2em',
+        }}
+        className='pad-s fsize-m'
+        type="number"
+        value={value}
+        onChange={(e) => onChange(parseInt(e.target.value))}
+      />
       <button className='pad-s fsize-m' onClick={() => onChange(value + 1)}>+1</button>
       <button className='pad-s fsize-m' onClick={() => onChange(value + 5)}>+5</button>
     </div>
