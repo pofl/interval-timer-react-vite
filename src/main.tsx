78900f9c1caacb2a23abc18441aa47eaff2c84be
Florian Polster <florian.polster@staffbase.com>
2025-03-16 01:42:51 +0100

    Port css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './interval.css'
 import { IntervalTimer } from './IntervalTimer.tsx'
 
 createRoot(document.getElementById('root')!).render(
